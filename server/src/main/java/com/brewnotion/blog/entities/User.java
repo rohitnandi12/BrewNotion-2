4575adcd9ba90ca3060864e9e0031405989098d7
Rohit Nandi <rohitnandi12@gmail.com>
2023-05-10 15:51:38 +0530

    Adds Post Entity and Relation

--- server/src/main/java/com/brewnotion/blog/entities/User.java
+++ server/src/main/java/com/brewnotion/blog/entities/User.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 @Table(name="users")
@@ -17,10 +20,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name="user_name", nullable = false, length = 100)
     private String name;
+
     private String email;
+
     private String password;
+
     private String about;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<>();
 
 }
